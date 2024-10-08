0bb334e5644a98793e0b52f7c3a9690ad619a742
S00237055 <S00237055@S-JL053S3>
2024-10-08 11:16:46 +0100

    Add project files.

---
+++ Lab2/Advertisment.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab2
+{
+    public class Advertisment
+    {
+        public int SellerID { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Sellers
+    {
+        public int SellerID { get; set; }
+        public string? SellerName { get; set; }
+    }
+
+    public class Categories
+    {
+        public int CategoriesID { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Lab2/AdvertismentDb.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2
+{
+    using Microsoft.EntityFrameworkCore;
+    public class AdvertismentDb : DbContext
+    {
+        public AdvertismentDb(DbContextOptions<AdvertismentDb> options) : base(options) { }
+
+        public DbSet<Advertisment> Advertisments => Set<Advertisment>();
+    }
+}
